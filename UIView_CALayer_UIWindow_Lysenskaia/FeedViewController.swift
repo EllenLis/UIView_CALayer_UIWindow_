6dc6935b59438c74d7036675c031c0cee79c1830
EllenLis <elena139@bk.ru>
2022-10-11 17:58:08 +0300

    Added gitignore file

---
+++ UIView_CALayer_UIWindow_Lysenskaia/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewController.swift
+//  UIView_CALayer_UIWindow_Lysenskaia
+//
+//  Created by ElenaL on 11.10.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .orange
+        
+    }
+    
+
+}
